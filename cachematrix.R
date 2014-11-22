122a4ab857a364c8d3b16ecc1fbf27667c982435
angelo biolcati rinaldi <biol@aria.to>
2014-11-23 00:35:06 +0100

    First Commit
    I just followed the mean example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As inverting a matrix is a costly operation,
+## we cache the result for future use 
+## to avoid a waste of computational resource.
 
-## Write a short comment describing this function
-
+## Givem a matrix, this function returns an obiect
+## which is ready to cache the result of the first call
+## to its solve method and return the cached value for further calls
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## This function uses the object created by the makeCacheMatrix function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
